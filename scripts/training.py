0d05d7a809c049d5a8902c94ca1e497040b9c6a1
enricovalguarnera <enry_v@hotmail.it>
2018-02-07 17:35:33 +0100

    [UPDATE] Model folders evaluate only.

--- scripts/training.py
+++ scripts/training.py
@@ -12,7 +12,10 @@ import re
 import my_mod_logs as log
 import my_mod_nets as nets
 
+MODEL_DIR = './models/'
 MANIPULATED_DIR = '/datasets/GTSRB/manipulated/'
+
+
 
 parser = argparse.ArgumentParser(description="Traffic signs classifier")
 #parser.add_argument('problem', help='The problem name (inside ./in folder)')
@@ -23,6 +26,7 @@ parser.add_argument("-d", "--dropout", help='', default='.3')
 parser.add_argument("-e", "--epochs", help='The number of epochs', default='80')
 parser.add_argument("-l", "--learning_rate", help='', default='1e-3')
 parser.add_argument("-n", "--net", help='The net you wanna use (LeNet, LeNet-adv, VGGnet)', default='LeNet')
+parser.add_argument("-m", "--model", help='', default='')
 parser.add_argument("--dataset", help='(online, pickle)', default='online')
 parser.add_argument('--debug', help='Print debug messages', action='store_true')
 parser.add_argument('--quiet', help='Print only the evaluation', action='store_true')
@@ -34,7 +38,9 @@ LR = float(args.learning_rate)
 BATCH_SIZE = int(args.batch_size)
 dropout = float(args.dropout)
 dataset_gtsrb = args.dataset
+model = args.model
 
+eval_only = False if model == '' else True
 debug = True if args.debug else False
 quiet = True if args.quiet else False
 augmentation = True if args.augmentation else False
@@ -45,6 +51,19 @@ print("epochs: {}".format(EPOCHS))
 print("learning rate: {}".format(LR))
 print("bath size: {}".format(BATCH_SIZE))
 print("dropout: {}".format(dropout))
+
+
+if not eval_only:
+    # new model folder
+    date = time.strftime("%Y-%m-%d_%H%M")
+    new_model_folder = "{}".format(date)
+    newpath = os.path.join(MODEL_DIR, new_model_folder)
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    model_lenet_path = os.path.join(newpath, 'lenet')
+else:
+    newpath = model
+
 
 # -----------------------------
 # create and load the log file
@@ -204,6 +223,7 @@ def predict(X_data):
 from sklearn.utils import shuffle
 from time import time
 
+
 # X_train = X_train_norm
 # X_valid = X_valid_norm
 # X_test = X_test_norm
@@ -218,54 +238,55 @@ errors = list()
 saver = tf.train.Saver()
 start = time()
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    num_examples = len(X_train)
+    if not eval_only:
+        sess.run(tf.global_variables_initializer())
+        num_examples = len(X_train)
 
-    log.log("Training... dropout = {} , batch_size = {} , learning rate = {}".format(dropout, BATCH_SIZE, LR), True)
-    print()
-    for i in range(EPOCHS):
-        try:
-            X_train, y_train = shuffle(X_train, y_train)
-#             print("Before Train %d sec"%(time() - start))
+        log.log("Training... dropout = {} , batch_size = {} , learning rate = {}".format(dropout, BATCH_SIZE, LR), True)
+        print()
+        for i in range(EPOCHS):
+            try:
+                X_train, y_train = shuffle(X_train, y_train)
+    #             print("Before Train %d sec"%(time() - start))
 
-            for offset in range(0, num_examples, BATCH_SIZE):
-                end = offset + BATCH_SIZE
-                batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-                sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1 - dropout, keep_prob_conv: 0.7})
-                # sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.5 , keep_prob_conv: 0.7})
+                for offset in range(0, num_examples, BATCH_SIZE):
+                    end = offset + BATCH_SIZE
+                    batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+                    sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1 - dropout, keep_prob_conv: 0.7})
+                    # sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 0.5 , keep_prob_conv: 0.7})
 
-#           print("After Train %d sec"%(time() - start))
+    #           print("After Train %d sec"%(time() - start))
 
-            validation_accuracy = evaluate(X_valid, y_valid)
-            training_accuracy = evaluate(X_train, y_train)
+                validation_accuracy = evaluate(X_valid, y_valid)
+                training_accuracy = evaluate(X_train, y_train)
 
-            errors.append((training_accuracy, validation_accuracy))
+                errors.append((training_accuracy, validation_accuracy))
 
-#           calculatiing minutes format
-            minutes = int((time() - start)/60)
-            seconds = ((time() - start) - (minutes*60))
+    #           calculatiing minutes format
+                minutes = int((time() - start)/60)
+                seconds = ((time() - start) - (minutes*60))
 
-            log.log("EPOCH %d - %d sec ....%d.%d min"%(i+1, time() - start,  minutes, seconds), True)
-            log.log("Training error = {:.3f} Validation error = {:.3f}".format(1- training_accuracy , 1- validation_accuracy), True)
+                log.log("EPOCH %d - %d sec ....%d.%d min"%(i+1, time() - start,  minutes, seconds), True)
+                log.log("Training error = {:.3f} Validation error = {:.3f}".format(1- training_accuracy , 1- validation_accuracy), True)
 
-            print()
+                print()
 
-#             print("After error computation %d sec"%(time() - start))
-            if i > 5 and i % 3 == 0:
-                saver.save(sess, './models/lenet')
-                print("Model saved %d sec"%(time() - start))
-        except KeyboardInterrupt:
-            print('Accuracy Model On Test Images: {}'.format(evaluate(X_test,y_test)))
-            break
+    #             print("After error computation %d sec"%(time() - start))
+                if i > 5 and i % 3 == 0:
+                    saver.save(sess, model_lenet_path)
+                    print("Model saved %d sec"%(time() - start))
+            except KeyboardInterrupt:
+                print('Accuracy Model On Test Images: {}'.format(evaluate(X_test,y_test)))
+                break
 
-    saver.save(sess, './models/lenet')
+        saver.save(sess, model_lenet_path)
 
 
 
 
 #Printing accuracy of the model on Training, validation and Testing set.
 with tf.Session() as sess:
-    saver.restore(sess, tf.train.latest_checkpoint('./models'))
+    saver.restore(sess, tf.train.latest_checkpoint(newpath))
     log.log("ACCURACY:", False)
     log.log('Accuracy Model On Training Images: {:.3f}'.format(evaluate(X_train, y_train)), False)
     log.log('Accuracy Model On Validation Images: {:.3f}'.format(evaluate(X_valid, y_valid)), False)
