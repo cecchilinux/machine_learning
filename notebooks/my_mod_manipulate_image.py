1a7cd4f02715f75fd275c70d6272da3abf22cb9c
Enrico Ceccolini <enrico.ceccolini@mail.com>
2018-02-14 13:11:54 +0100

    [REPORT] blurring

--- notebooks/my_mod_manipulate_image.py
+++ notebooks/my_mod_manipulate_image.py
@@ -1,64 +1,8 @@
-
 import numpy as np
 import cv2
 from skimage import exposure # a collection of algorithms for image processing.
 import warnings
 warnings.filterwarnings("ignore")
-
-
-def sharpen_img(img):
-    gb = cv2.GaussianBlur(img, (5,5), 20.0)
-    return cv2.addWeighted(img, 2, gb, -1, 0)
-
-def transform_img(img_in):
-    img_in = img_in.copy()
-    img_out = sharpen_img(img_in)
-    img_out = cv2.cvtColor(img_in, cv2.COLOR_RGB2YUV)
-
-    img_out[:,:,0] = cv2.equalizeHist(img_out[:,:,0])
-
-    return img_out[:,:,0]
-
-def random_rotate_img(img):
-    c_x,c_y = int(img.shape[0]/2), int(img.shape[1]/2)
-    ang = 30.0*np.random.rand()-15
-    Mat = cv2.getRotationMatrix2D((c_x, c_y), ang, 1.0)
-    return cv2.warpAffine(img, Mat, img.shape[:2])
-
-def random_scale_img(img):
-    img2=img.copy()
-    sc_y=0.4*np.random.rand()+1.0
-    img2=cv2.resize(img, None, fx=1, fy=sc_y, interpolation = cv2.INTER_CUBIC)
-
-    dy = int((img2.shape[1]-img.shape[0])/2)
-    end = img.shape[1]-dy
-    img2 = img2[dy:end,:,:]
-    assert img2.shape[0] == 32
-#     print(img2.shape,dy,end)
-    return img2
-
-#Compute linear image transformation ing*s+m
-def lin_img(img,s=1.0,m=0.0):
-    img2=cv2.multiply(img, np.array([s]))
-    return cv2.add(img2, np.array([m]))
-
-#Change image contrast; s>1 - increase
-def contr_img(img, s=1.0):
-    m = 127.0*(1.0-s)
-    return lin_img(img, s, m)
-
-
-def augment_img(img):
-    img = img.copy()
-    img = contr_img(img, 1.8*np.random.rand()+0.2)
-    img = random_rotate_img(img)
-    img = random_scale_img(img)
-
-    # return transform_img(img)
-    return normalize_img(img)
-
-
-
 
 # -----------------------
 # normalization
@@ -66,8 +10,6 @@ def augment_img(img):
 
 def normalize_img(img):
 
-    # img_bl = motion_blur(img)
-    #img_bl = sharpen_img(img)
     img_y = cv2.cvtColor(img, (cv2.COLOR_RGB2YUV))[:,:,0] #converte l'immagine in YUV e tiene il canale Y
 
     # ----- global equalization
@@ -76,24 +18,20 @@ def normalize_img(img):
     img_y = (img_y / 255.).astype(np.float32) # rappresenta i valori in un range di [0-1]
 
 
-    # ----- local equalization: use one of the follow
-    # adjust_log : very very fast,
-    # equalize_adapthist : really slow but accuracy gain
-    #img_y = exposure.adjust_log(img_y)
+    # ----- local equalization
 
     # An algorithm for local contrast enhancement, that uses histograms computed
     # 0 over different tile regions of the image. Local details can therefore be
     # enhanced even in regions that are darker or lighter than most of the image.
     img_y = (exposure.equalize_adapthist(img_y) - 0.5)
-    # -----
-    img_y = img_y.reshape(img_y.shape + (1,))
 
+    img_y = img_y.reshape(img_y.shape + (1,))
 
     return img_y
 
 
 # ------------------------
-# blur
+# blurring
 # ------------------------
 
 def motion_blur(img):
